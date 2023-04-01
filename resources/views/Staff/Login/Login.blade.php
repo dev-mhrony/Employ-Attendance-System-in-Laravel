2c090ac8041411fcaf0508c3a5bb0da7b1a08f08
dev.mhrony <mh.rony.dhanvi@gmail.com>
2023-04-01 18:03:43 +0600

    Last update - 01/04/2023

--- resources/views/Staff/Login/Login.blade.php
+++ resources/views/Staff/Login/Login.blade.php
@@ -3,6 +3,27 @@
 @section('Content')
 <div class="container-fluid p-0">
   <div class="row m-0">
+    <div class="col-4 p-5">
+    <div style="height: calc(100vh - 100px)">
+      <form id="login-user-form" class="pt-5" action="{{url('/')}}" method="post">
+        @csrf
+        <p class="text-center font-weight-bold mt-1 tx" style="font-size: 110%">LOGIN</p>
+        <hr>
+        <p class="fz95 mb-1">Phone</p>
+        <input type="number" name="phone" class="form-control w-100" required>
+
+        <p class="fz95 mt-2 mb-1">Password</p>
+        <input type="password" name="password" class="form-control w-100" required>
+
+        <p class="float-right fz95 mt-2 tx cs">Forgot password</p>
+        <button type="submit" class="btn bg w-100 text-white cs">Log in</button>
+        @if (\Session::has('msg'))
+        <p class="text-danger mt-2 text-center mb-0 fz-95">{!! \Session::get('msg') !!}</p>
+        @endif
+
+        </form>
+      </div>
+    </div>
     <div class="col-8 p-5 bg text-white">
       <p class="text-center font-weight-bold mt-0 mb-5" style="font-size: 180%;">Building a work management and administration system</p>
       <div class="row m-0">
@@ -30,27 +51,7 @@
 
      
    </div>
-   <div class="col-4 p-5">
-    <div style="height: calc(100vh - 100px)">
-      <form id="login-user-form" class="pt-5" action="{{url('/')}}" method="post">
-        @csrf
-        <p class="text-center font-weight-bold mt-1 tx" style="font-size: 110%">LOGIN</p>
-        <hr>
-        <p class="fz95 mb-1">Phone</p>
-        <input type="number" name="phone" class="form-control w-100" required>
-
-        <p class="fz95 mt-2 mb-1">Password</p>
-        <input type="password" name="password" class="form-control w-100" required>
-
-        <p class="float-right fz95 mt-2 tx cs">Forgot password</p>
-        <button type="submit" class="btn bg w-100 text-white cs">Log in</button>
-        @if (\Session::has('msg'))
-        <p class="text-danger mt-2 text-center mb-0 fz-95">{!! \Session::get('msg') !!}</p>
-        @endif
-
-      </form>
-    </div>
-  </div>
+  
 </div>
 </div>
 <script src="{{ asset('index/js/jquery-3.6.0.js') }}"></script>
